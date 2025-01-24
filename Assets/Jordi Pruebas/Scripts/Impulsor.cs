40bc548d7a2dc43d8df5435df289bffd20fddbfb
JordiLlinaresSanchez <149331069+JordiLlinaresSanchez@users.noreply.github.com>
2025-01-24 23:24:31 +0100

    impulsor

--- Assets/Jordi Pruebas/Scripts/Impulsor.cs
+++ Assets/Jordi Pruebas/Scripts/Impulsor.cs
@@ -5,20 +5,47 @@ using UnityEngine;
 public class Impulsor : MonoBehaviour
 {
     [SerializeField]
-    int force = 1;
+    float _force = 1;
+    [SerializeField]
+    float _impulseTime = 1.0f;
+    Transform _transform;
+
+    Rigidbody2D _myRigidbody;
+    [SerializeField]
+    bool _isJumping = false, _haveFuel = true;
+    float _actualFuelTime = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _transform = transform;
+        _myRigidbody = GetComponentInParent<Rigidbody2D>();
+        Debug.Log(_myRigidbody);
     }
 
     public void SetImpulse(bool isImpulsing)
     {
+        _isJumping = isImpulsing;
+    }
 
+    private void Impulse()
+    {
+        _myRigidbody.AddForce(new Vector2(_transform.up.x, _transform.up.y).normalized*_force);
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if(_isJumping && _haveFuel)
+        {
+            Impulse();
+
+            _actualFuelTime += Time.deltaTime;
+            _haveFuel = _impulseTime > _actualFuelTime;
+        }
+        else if(!_haveFuel)
+        {
+            _isJumping = false;
+            _actualFuelTime = 0.0f;
+            _haveFuel = true;
+        }
     }
 }
