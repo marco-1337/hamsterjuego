4932b3f01d471259f8850dc3b6c6a00cfb7d1baa
JordiLlinaresSanchez <149331069+JordiLlinaresSanchez@users.noreply.github.com>
2025-01-26 10:06:55 +0100

    sonido del menu

--- Assets/UI/UI_StartGame.cs
+++ Assets/UI/UI_StartGame.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UI_StartGame : MonoBehaviour
 {
+    [SerializeField] UnityEvent startMusic;
     private void Start()
     {
         GetComponent<Button>().onClick.AddListener(StartGameScene);
+        startMusic.Invoke();
     }
 
     private void StartGameScene()
