62061fd79f3a956dd55c2ad098e48a92206b6b33
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-01-26 13:42:27 +0100

    cambios a pipas

--- Assets/Scripts/Pipa/PipaComponent.cs
+++ Assets/Scripts/Pipa/PipaComponent.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PipaComponent : MonoBehaviour
 {
     [SerializeField] Pipas contadorPipas;
-    private void OnTriggerEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.GetComponent<MovementComponent>() != null)
         {
