5b1d498257dd802fbeb4ffc375e193f541b02ead
Diego <dicaball@ucm.es>
2025-01-27 19:31:50 +0100

    Ya no debe clipear el tubo y el ground detector se ha limpiado

--- Assets/Scripts/Movement/GroundDetector.cs
+++ Assets/Scripts/Movement/GroundDetector.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class GroundDetector : MonoBehaviour
 {
     [SerializeField] float distance = 0;
-    [SerializeField] Collider2D myCollider;
-    [SerializeField] PolygonCollider2D rocketCollider;
-
-    [SerializeField] Collider2D music1, music2, music3;
+    [SerializeField] Collider2D[] _colliders;
     
     public bool IsGrounded()
     {
@@ -17,35 +14,12 @@ public class GroundDetector : MonoBehaviour
         bool grounded = false;
         while(i < allTargets.Length && !grounded)
         {
-            if(allTargets[i].collider != myCollider 
-                && allTargets[i].collider != rocketCollider
-                && allTargets[i].collider != music1
-                && allTargets[i].collider != music3
-                && allTargets[i].collider != music2) grounded = true;
+            int j = 0;
+            while (j < _colliders.Length && allTargets[i].collider != _colliders[j]) j++;
+            grounded = j == _colliders.Length;
             ++i;
         }
         return grounded;
-    }
-
-    public Vector2 SurfaceVector()
-    {
-        RaycastHit2D[] allTargets = Physics2D.RaycastAll(transform.position, Vector2.down, distance);
-        int i = 0;
-
-        while (i < allTargets.Length)
-        {
-            if (allTargets[i].collider != myCollider 
-                && allTargets[i].collider != rocketCollider
-                && allTargets[i].collider != music1
-                && allTargets[i].collider != music3
-                && allTargets[i].collider != music2)
-            {
-                return allTargets[i].collider.GetComponent<Transform>().right;
-            }
-            ++i;
-        }
-
-        return Vector2.zero;
     }
 
     private void OnDrawGizmos()
