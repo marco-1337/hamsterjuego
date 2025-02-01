22fecab1a51b6a90a1fddd74310d597ae60d671e
Diego <dicaball@ucm.es>
2025-02-01 14:20:44 +0100

    Grounded chulo

--- Assets/Scripts/Movement/GroundDetector.cs
+++ Assets/Scripts/Movement/GroundDetector.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GroundDetector : MonoBehaviour
 {
-    [SerializeField] float distance = 0;
+    [SerializeField] float _distance = 0;
+    [SerializeField] int _rays = 1;
+    [SerializeField] float _width = 1f;
 
     private LayerMask _groundMask;
 
@@ -13,10 +16,17 @@ public class GroundDetector : MonoBehaviour
         _groundMask = LayerMask.GetMask("Ground");
     }
 
-    public bool IsGrounded() => (bool)Physics2D.Raycast(transform.position, Vector2.down, distance, _groundMask);
+    public bool IsGrounded()
+    {
+        float unit = _width / _rays;
+        int i = 0;
+        while (i <= _rays && !(bool)Physics2D.Raycast((new Vector2(transform.position.x - _width / 2 + unit * i, transform.position.y)), Vector2.down, _distance, _groundMask)) ++i;
+        return i<= _rays;
+    }
 
     private void OnDrawGizmos()
     {
-        Debug.DrawRay(transform.position, Vector2.down * distance, Color.red);
+        float unit = _width / _rays;
+        for(int i = 0; i <= _rays; ++i) Debug.DrawRay(new Vector2(transform.position.x - (_width / 2) + unit * i, transform.position.y), Vector2.down * _distance, Color.red);
     }
 }
